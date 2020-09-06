9725c776121a197c0a78f0435e962b53bc554089
YangGeYangGe <171951502@qq.com>
2020-09-06 19:18:39 +0800

    bubble sort

---
+++ sort/bubble_sort.py
@@ -0,0 +1,14 @@
+def BubbleSort(l:list):
+    n = len(l)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if l[j] > l[j + 1]:
+                temp = l[j]
+                l[j] = l[j + 1]
+                l[j + 1] = temp
+    return l
+
+
+if __name__ == '__main__':
+    l = [3,5,1,2,547,6,12,65,2,7,1347,23]
+    print(BubbleSort(l))
